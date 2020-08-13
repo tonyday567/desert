49f4f970f734eb79bc1b9a8197f33361a7e245a4
Tony Day <tonyday567@gmail.com>
2020-08-14 09:08:59 +1000

    Working

--- src/Desert.hs
+++ src/Desert.hs
@@ -12,6 +12,7 @@ import Codec.Picture.Types
 import qualified Data.ByteString as B
 import qualified Data.Vector.Storable as V
 import Control.Lens
+import Data.List ((!!))
 
 -- | Turn a PNG into a [Chart Double]
 --
@@ -33,22 +34,24 @@ toColour = (\(PixelRGB8 r g b) -> Colour (fromIntegral r/256) (fromIntegral g/25
 
 --  Rectangle chart made of single rectangles of colour.
 fillColor :: [[Colour]] -> [Chart Double]
-fillColor css =
-  zipWith
-  (\c (x', y') ->
-      Chart (RectA (RectStyle 0 transparent c))
-      [RectXY (Rect (fromIntegral x') (fromIntegral x'+1) (fromIntegral y') (fromIntegral y' + 1))])
-  (mconcat css)
-  ((,) <$>
-   [0..height - 1::Int] <*>
-   [0..width' - 1::Int])
+fillColor css = 
+  (\(x, y) ->
+      Chart (RectA (RectStyle 0 transparent ((css !! x) !! y)))
+      [RectXY (Rect
+               (fromIntegral x)
+               (fromIntegral x+1)
+               (fromIntegral y)
+               (fromIntegral y + 1))]) <$> pts
   where
-    height = length css
-    width' = maybe 0 length (head css)
+    h = length css
+    w = maybe 0 length (head css)
+    pts = (,) <$>
+      (take h [(0 :: Int)..]) <*>
+      (take w [(0 :: Int)..])
 
 -- | reverses the top to bottom convention
 crop :: Rect Int -> [[Colour]] -> [[Colour]]
-crop (Rect x z y w) css = fmap (take (w-y)) . take (z - x) . fmap (drop y) . drop x $ css
+crop (Rect x z y w) css = fmap (take (w - y) . drop y) . take (z - x) . drop x $ css
 
 go :: IO ()
 go = do
@@ -61,7 +64,7 @@ go = do
     defaultHudOptions []
     (fillColor $ crop r $ fromImage i)
     where
-      r@(Rect x z y w) = Rect (0::Int) 300 0 200
+      r@(Rect x z y w) = Rect (0::Int) 372 0 436
       h = w - y
       w' = z - x
       asp = fromIntegral w' / fromIntegral h
@@ -69,8 +72,8 @@ go = do
 fromImage :: Image PixelRGB8 -> [[Colour]]
 fromImage (Image w h v) =
   -- from top to bottom
-  generate h (\y ->
-    generate w (\x -> let i = (x + (h - y) * w) * 3 in
+  generate w (\x ->
+    generate h (\y -> let i = (x + (h - y - 1) * w) * 3 in
                    Colour
                    (toFloat $ v V.! i)
                    (toFloat $ v V.! (i+1))
